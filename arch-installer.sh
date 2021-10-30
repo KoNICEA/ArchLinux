498fff5802d365b345165c781778698003936a77
Enrico Abello <36589032+CCraed@users.noreply.github.com>
2021-10-31 00:22:46 +0200

    Add files via upload

--- arch-installer.sh
+++ arch-installer.sh
@@ -1,11 +1,81 @@
 #!/bin/bash
 
+# --------------------
+# |||| Functions |||||
+# --------------------
+function gen-arch-chroot {
+echo "----------------------------------"
+echo " -- Generate ArchChroot Script --"
+echo "----------------------------------"
+sleep 3
+
+V1='$NUSR'
+V2='$HSTN'
+cat <<EOF > /mnt/root/complete.sh
+    echo "----------------------------"
+    echo " -- Install BasePackages --"
+    echo "----------------------------"
+    sleep 3
+    pacman -S --needed grub efibootmgr sudo
+
+    echo "-----------------------------"
+    echo " -- ArchLinux BaseConfigs --"
+    echo "-----------------------------"
+    sleep 3
+    timedatectl set-timezone Europe/Rome
+    ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+    hwclock --systohc
+    mv /etc/locale.gen /etc/locale.gen.all
+    echo it_IT.UTF-8 UTF-8 > /etc.locale.gen
+    locale-gen
+    echo LANG=it_IT.UTF-8 > /etc/locale.conf
+    echo KEYMAP=it > /etc/vconsole.conf
+    echo -n "HostName >> "
+    read HSTN
+    echo $V2 > /etc/hostname
+    nano /etc/hosts
+
+    echo "-------------------------------"
+    echo " -- Insert Password of Root --"
+    echo "-------------------------------"
+    passwd
+    grub-install --target=x86_64-efi --bootloader-id=UEFI-CPH-LNX --efi-directory=/boot/efi
+    grub-mkconfig -o /boot/grub/grub.cfg
+    mkinitcpio -P
+
+    echo "----------------------------"
+    echo " -- Create User and Home --"
+    echo "----------------------------"
+    sleep 1
+    echo -n "UserName of New User >> "
+    read NUSR
+    useradd --create-home $V1
+    passwd $V1
+    usermod --append --groups wheel $V1
+    nano /etc/sudoers
+
+    echo "---------------------------"
+    echo " -- Complete.Sh in Home --"
+    echo "---------------------------"  
+    echo "pacman -Syu --needed xorg-server nvidia nvidia-utils nvidia-settings xorg-xinit xorg-xrandr xorg-xsetroot neofetch neovim unzip git htop dmenu chromium firefox xterm ntfs-3g " >> /home/$V1/complete.sh
+    echo "pacman -S " >> /home/$V1/complete.sh
+    echo "pacman -S networkmanager rp-pppoe wpa_supplicant wireless_tools networkmanager-strongswan nm-connection-editor" >> /home/$V1/complete.sh
+    echo "pacman -S sudo pacman -S ttf-hanazono ttf-sazanami" >> /home/$V1/complete.sh
+    echo "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si" >> /home/$V1/complete.sh
+    chmod +x /home/$V1/complete.sh
+EOF
+}
+
+# ---------------
+# |||| Main |||||
+# ---------------
 EFIVARS=/sys/firmware/efi/efivars
 if [[ -d "$EFIVARS" ]]
 then
     echo "-----------------------------"
     echo " -- Partition Disks --"
-    echo "-----------------------------"    
+    echo "-----------------------------"
+    sleep 3
     LSD=$(lsblk -o NAME,SIZE,MODEL)
     echo -e "\033[0;33m$LSD\033[0m"
     echo -n "Select Disk To Install Arch >> "
@@ -22,7 +92,8 @@ then
 
     echo "-------------------------"
     echo " -- Install ArchLinux --"
-    echo "-------------------------"    
+    echo "-------------------------"
+    sleep 3   
     mount "/dev/$SDK"2 /mnt
     pacstrap /mnt base linux linux-firmware	
     mkdir /mnt/boot/efi
@@ -31,63 +102,11 @@ then
 
     echo "----------------"
     echo " -- GenFstab --"
-    echo "----------------"    
+    echo "----------------"
+    sleep 1
     genfstab -U /mnt >> /mnt/etc/fstab
-
-
-cat <<EOF > /mnt/tmp/complete.sh
-    echo "----------------------------"
-    echo " -- Install BasePackages --"
-    echo "----------------------------"
-    pacman -S grub efibootmgr
-
-    echo "-----------------------------"
-    echo " -- ArchLinux BaseConfigs --"
-    echo "-----------------------------"
-    timedatectl list-timezones
-    timedatectl set-timezone Europe/Rome
-    ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
-    hwclock --systohc
-    mv /etc/locale.gen /etc/locale.gen.all
-    echo it_IT.UTF-8 UTF-8 > /etc.locale.gen
-    locale-gen
-    echo LANG=it_IT.UTF-8 > /etc/locale.conf
-    echo KEYMAP=it > /etc/vconsole.conf
-    echo DSK-CPH-EAP > /etc/hostname
-    nano /etc/hosts
-
-    echo "-------------------------------"
-    echo " -- Insert Password of Root --"
-    echo "-------------------------------"
-    passwd
-    grub-install --target=x86_64-efi --bootloader-id=UEFI-CPH-LNX --efi-directory=/boot/efi
-    grub-mkconfig -o /boot/grub/grub.cfg
-    mkinitcpio -P
-
-    echo "----------------------------"
-    echo " -- Create User and Home --"
-    echo "----------------------------"
-    echo -n "UserName of New User >> "    
-    read NUSR
-    useradd --create-home $NUSR
-    passwd $NUSR
-    usermod --append --groups wheel $NUSR
-    nano /etc/sudoers
-
-    echo "---------------------------"
-    echo " -- Complete.Sh in Home --"
-    echo "---------------------------"  
-    echo "pacman -Syu xorg-server nvidia nvidia-utils nvidia-settings xorg-xinit xorg-xrandr xorg-xsetroot neofetch neovim unzip git htop dmenu chromium firefox xterm ntfs-3g" >> /home/$NUSR/complete.sh
-    echo "pacman -S feh python python-pip ruby keepassxc mplayer mpd openssh openvpn" >> /home/$NUSR/complete.sh
-    echo "pacman -S networkmanager rp-pppoe wpa_supplicant wireless_tools networkmanager-strongswan nm-connection-editor" >> /home/$NUSR/complete.sh
-    echo "pacman -S sudo pacman -S ttf-hanazono ttf-sazanami" >> /home/$NUSR/complete.sh
-    echo "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si" >> /home/$NUSR/complete.sh
-    chmod +x /home/$NUSR/complete.sh
-
-exit # to leave the chroot
-EOF
-
-    arch-chroot /mnt /tmp/complete.sh
+    gen-arch-chroot
+    arch-chroot /mnt /root/complete.sh
 
 else
     echo "Reboot in UEFI..."
